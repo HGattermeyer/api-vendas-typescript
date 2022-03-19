847ba1ee0fc541fafc3b42a4bfd936725311dbec
Henrique <henrique1989@gmail.com>
2022-03-19 12:46:57 +0000

    Implemented rate limiter

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,19 +1,22 @@
-import 'reflect-metadata';
-import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
 import cors from 'cors';
 import { errors } from 'celebrate';
 import { pagination } from 'typeorm-pagination';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
-import '@shared/typeorm';
 import uploadConfig from '@config/upload';
+import rateLimiter from './middlewares/rateLimiter';
+import 'reflect-metadata';
+import 'dotenv/config';
+import 'express-async-errors';
+import '@shared/typeorm';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(rateLimiter);
 
 app.use(pagination);
 
